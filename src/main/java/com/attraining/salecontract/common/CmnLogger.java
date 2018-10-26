eed4b95c6afe4c74a08109b557743cd920c17739
spiderxman <3130569@qq.com>
2018-10-26 23:30:59 +0900

    20181026 log4j2追加

--- src/main/java/com/attraining/salecontract/common/CmnLogger.java
+++ src/main/java/com/attraining/salecontract/common/CmnLogger.java
@@ -1,57 +1,58 @@
 package com.attraining.salecontract.common;
 
-import javax.servlet.http.HttpSession;
 
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
+
+import javax.servlet.http.HttpSession;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 public class CmnLogger {
 
-    /* リクエストが多重の場合、ログレベルを操作する */
-    private Integer requestAllowanceCnt = 1;
+	/* リクエストが多重の場合、ログレベルを操作する */
+	private Integer requestAllowanceCnt = 1;
 
-    private static String FQCN = CmnLogger.class.getName();
-    private static Logger logger = Logger.getLogger(CmnLogger.class);
-
+	private static Logger logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
+	
     public void error(Object message) {
-        logger.log(FQCN, Level.ERROR, message, null);
+    	logger.error(message);
     }
-
+    
     public void error(Object message, HttpSession session) {
-        error(message, null, session);
+    	error(message, null, session);
     }
 
     public void error(Object message, Throwable t) {
-        logger.log(FQCN, Level.ERROR, message, t);
+        logger.error(message, t);
     }
 
     public void error(Object message, Throwable t, HttpSession session) {
-        if((Integer)session.getAttribute("requestCnt") > requestAllowanceCnt){
-            // リクエストが多重の場合はWARNで出力する
-            logger.log(FQCN, Level.WARN, message, t);
-        }else{
-            // リクエストがシングルの場合は通常通りERRORで出力する
-            logger.log(FQCN, Level.ERROR, message, t);
-        }
+    	if((Integer)session.getAttribute("requestCnt") > requestAllowanceCnt){
+    		// リクエストが多重の場合はWARNで出力する
+    		logger.warn(message, t);
+    	}else{
+    		// リクエストがシングルの場合は通常通りERRORで出力する
+    		logger.error(message, t);
+    	}
     }
 
     public void warn(Object message, Throwable t) {
-        logger.log(FQCN, Level.WARN, message, t);
+    	logger.warn(message, t);
     }
-
+    
     public void warn(Object message) {
-        logger.log(FQCN, Level.WARN, message, null);
+    	logger.warn(message);
     }
 
     public void info(Object message) {
-        logger.log(FQCN, Level.INFO, message, null);
+    	logger.info(message);
     }
 
     public void debug(Object message) {
-        logger.log(FQCN, Level.DEBUG, message, null);
+    	logger.debug(message);
     }
 
     public void debug(Object message, Throwable t) {
-        logger.log(FQCN, Level.DEBUG, message, null);
+    	logger.debug(message, t);
     }
 }
--- src/main/java/com/attraining/salecontract/controller/SCommon_LOGINController.java
+++ src/main/java/com/attraining/salecontract/controller/SCommon_LOGINController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.attraining.salecontract.bean.UserInfo;
+import com.attraining.salecontract.common.CmnLogger;
 import com.attraining.salecontract.common.Message;
 import com.attraining.salecontract.common.PropertiesFileLoader;
 import com.attraining.salecontract.service.SUserService;
@@ -24,74 +25,75 @@ import com.attraining.salecontract.service.SUserService;
  * <pre>
  * [機 能] ユーザーログイン。
  * [説 明] ユーザーログインController
- * @author [作 成] 2018/10/01 東山(ITT)
+ * &#64;author [作 成] 2018/10/01 東山(ITT)
  * </pre>
  */
 @Controller
 public class SCommon_LOGINController {
+	// Loggerインスタンス
+	private static CmnLogger logger = new CmnLogger();
+	@Autowired
+	SUserService sUserService;
 
-    @Autowired
-    SUserService sUserService;
+	/**
+	 * <pre>
+	 * [機 能]ユーザーログイン処理
+	 * [説 明]取得したユーザー情報によって画面遷移します
+	 * &#64;param userId ユーザーＩＤ
+	 * &#64;param password パスワード
+	 * &#64;return String 	遷移先
+	 * </pre>
+	 */
+	@RequestMapping("/Login")
+	public ModelAndView getUser(String userId, String password, ModelAndView mv, HttpSession session) {
+		logger.info("★★★★★★login start★★★★★★");
+		// 初回ログイン画面表示判断
+		// ユーザーＩＤかつ、パスワードnullの場合
+		if (null == userId && null == password) {
+			// セッション中身の情報を削除
+			session.removeAttribute("userName");
+			// ログイン画面へ遷移設定
+			mv.setViewName("Common/Login");
+			return mv;
+		}
 
+		// 処理ロジック
+		// ユーザ検索処理を行う
+		List<UserInfo> userInfoList = sUserService.getUserInfoList(userId, password);
+		// 件数>0場合
+		if (userInfoList.size() > 0) {
+			// メッセージを設定
+			mv.addObject("message", new Message("I", PropertiesFileLoader.getProperty("info.system.normal")));
+			// ユーザー名をセッションインスタンスに設定
+			session.setAttribute("loginUserInfo", userInfoList.get(0));
+			// メニュー画面遷移前処理
+			// mv.setView(new RedirectView("MainMenu"));
+			mv.setViewName("Common/MainMenu");
+			// 返却件数=0の場合
+		} else {
+			// メッセージを設定
+			mv.addObject("message", new Message("E", PropertiesFileLoader.getProperty("errors.login_noexist")));
+			mv.addObject("userId", userId);
+			mv.addObject("password", password);
+			// 画面IDを設定
+			mv.setViewName("Common/Login");
+			// ログを出力
+		}
 
-    /**
-     * <pre>
-     * [機 能]ユーザーログイン処理
-     * [説 明]取得したユーザー情報によって画面遷移します
-     * @param userId ユーザーＩＤ
-     * @param password パスワード
-     * @return String 	遷移先
-     * </pre>
-     */
-    @RequestMapping("/Login")
-    public ModelAndView  getUser(String userId, String password, ModelAndView mv, HttpSession session) {
-        // 初回ログイン画面表示判断
-        // ユーザーＩＤかつ、パスワードnullの場合
-        if(null == userId && null == password) {
-            // セッション中身の情報を削除
-            session.removeAttribute("userName");
-            // ログイン画面へ遷移設定
-            mv.setViewName("Common/Login");
-            return mv;
-        }
+		return mv;
+	}
 
-        // 処理ロジック
-        // ユーザ検索処理を行う
-        List<UserInfo> userInfoList = sUserService.getUserInfoList(userId,password);
-        // 件数>0場合
-        if (userInfoList.size() > 0) {
-            // メッセージを設定
-            mv.addObject("message", new Message("I", PropertiesFileLoader.getProperty("info.system.normal")));
-            // ユーザー名をセッションインスタンスに設定
-            session.setAttribute("loginUserInfo", userInfoList.get(0));
-            // メニュー画面遷移前処理
-            //mv.setView(new RedirectView("MainMenu"));
-            mv.setViewName("Common/MainMenu");
-        // 返却件数=0の場合
-        }else {
-            // メッセージを設定
-            mv.addObject("message", new Message("E", PropertiesFileLoader.getProperty("errors.login_noexist")));
-            mv.addObject("userId", userId);
-            mv.addObject("password", password);
-            // 画面IDを設定
-            mv.setViewName("Common/Login");
-            // ログを出力
-        }
-
-        return mv;
-    }
-
-    /**
-     * <pre>
-     * [機 能]メイン画面遷移処理
-     * [説 明]メイン画面遷移します
-     * @param userId ユーザーＩＤ
-     * @return String 	遷移先
-     * </pre>
-     */
-    @RequestMapping("/MainMenu")
-    public ModelAndView  mainMenu(ModelAndView mv) {
-            mv.setViewName("Common/MainMenu");
-        return mv;
-    }
+	/**
+	 * <pre>
+	 * [機 能]メイン画面遷移処理
+	 * [説 明]メイン画面遷移します
+	 * &#64;param userId ユーザーＩＤ
+	 * &#64;return String 	遷移先
+	 * </pre>
+	 */
+	@RequestMapping("/MainMenu")
+	public ModelAndView mainMenu(ModelAndView mv) {
+		mv.setViewName("Common/MainMenu");
+		return mv;
+	}
 }
